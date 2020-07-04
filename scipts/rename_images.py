0f8038543481e8cc3f8772b8fc0914f419e291ef
Satvik Choudhary <satvikchoudhary@gmail.com>
2020-07-05 02:27:35 +0530

    Removed duplicate

--- scipts/rename_images.py
+++
@@ -1,9 +0,0 @@
-import os
-data_dir = '../data/'
-img_names = os.listdir(data_dir)
-total_images = len(img_names)
-
-for i, img_name in enumerate (img_names):
-    name, ext = img_name.split('.')
-    new_name = 'img_{:02d}.{}'.format(i + 1, ext)
-    os.rename(data_dir + img_name, data_dir + new_name)
